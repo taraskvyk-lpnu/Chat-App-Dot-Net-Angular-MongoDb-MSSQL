b5f9c6255c8b93032dcd97e01e21dd20f9e3c244
TarasKvyk <tasik981@gmail.com>
2024-08-22 20:36:30 +0300

    Implemented chat repository pattern

---
+++ ChatManagement.API/ChatManagement.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using ChatManagement.DataAccess;
+using ChatManagement.Domain.Repositories;
+using ChatManagement.Infrastructure.CustomException;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatManagement.Infrastructure.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    protected readonly ChatManagementDbContext _chatContext;
+    protected readonly DbSet<TEntity> _dbSet;
+
+
+    public Repository(ChatManagementDbContext chatContext)
+    {
+        _chatContext = chatContext;
+        _dbSet = _chatContext.Set<TEntity>();
+    }
+    
+    public async Task<TEntity> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public async Task RemoveByIdAsync(Guid id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+
+        if (entity != null)
+        {
+            throw new NotFoundException("Entity not found");
+        }
+        
+        _dbSet.Remove(entity);
+    }
+}
---
+++ ChatManagement.API/ChatManagement.Domain/Repositories/IChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using ChatManagement.Domain.Models;
+using ChatManagement.Domain.Models.Dtos;
+
+namespace ChatManagement.Domain.Repositories;
+
+public interface IChatRepository : IRepository<Chat>
+{
+    Task AttachUserToChatAsync(Guid chatId, Guid userId);
+    Task DetachUserFromChatAsync(Guid chatId, Guid userId);
+    Task<IEnumerable<Chat>> GetChatsByUserIdAsync(Guid userId);
+    //Task<IEnumerable<User>> GetUsersByChatIdAsync(string chatId);
+    Task UpdateChatAsync(ChatDto chatDto);
+}
