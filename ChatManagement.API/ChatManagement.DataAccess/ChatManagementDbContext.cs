708cb6eea1d0d3e8faf9bf4ab74cef5c70dc89d9
TarasKvyk <tasik981@gmail.com>
2024-08-22 19:51:32 +0300

    Implemented ChatManagement.DataAccess

---
+++ ChatManagement.API/ChatManagement.DataAccess/ChatManagementDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ChatManagement.DataAccess.Configurations;
+using ChatManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatManagement.DataAccess;
+
+public class ChatManagementDbContext : DbContext
+{
+    public ChatManagementDbContext(DbContextOptions<ChatManagementDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Chat> Chats { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new ChatConfiguration());
+    }
+}
---
+++ ChatManagement.API/ChatManagement.DataAccess/Configurations/ChatConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ChatManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatManagement.DataAccess.Configurations;
+
+public class ChatConfiguration : IEntityTypeConfiguration<Chat>
+{
+    public void Configure(EntityTypeBuilder<Chat> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(c => c.Title)
+            .IsRequired()
+            .HasMaxLength(256);
+    }
+}
