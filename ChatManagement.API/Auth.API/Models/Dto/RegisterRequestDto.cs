6ec477fc33bf6b4e6829f365c50cf079d7094780
TarasKvyk <tasik981@gmail.com>
2024-08-27 16:54:31 +0300

    Added attributes to login and register request models

--- ChatManagement.API/Auth.API/Models/Dto/RegisterRequestDto.cs
+++ ChatManagement.API/Auth.API/Models/Dto/RegisterRequestDto.cs
@@ -2,14 +2,25 @@
 
 namespace Auth.API.Models.Dto
 {
-    public class LoginRequest
+    
+    public class RegisterRequestDto
     {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters.")]
+        public string Name { get; set; }
+
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Phone number is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        public string PhoneNumber { get; set; }
+
         [Required(ErrorMessage = "Password is required.")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
+
+        public string? Role { get; set; }
     }
 }
