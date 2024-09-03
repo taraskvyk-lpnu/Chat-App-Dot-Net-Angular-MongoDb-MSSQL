4aeb5604ef7641bf3e53c317d5b468350feb2c99
TarasKvyk <tasik981@gmail.com>
2024-09-03 20:35:15 +0300

    Added Extension methods to use in Program.cs

---
+++ ChatManagement.API/ChatMessaging/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using ChatMessaging.Contracts;
+using ChatMessaging.DataAccess;
+using ChatMessaging.Implementations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace ChatMessaging.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddChatServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ChatDbContext>();
+        builder.Services.AddScoped<IMessageRepository, MongoMessageRepository>();
+        builder.Services.AddSignalR();
+    }
+
+    public static void AddJwtAuth(this WebApplicationBuilder builder)
+    {
+        var jwtOptions = builder.Configuration.GetSection("ApiSettings:JwtOptions").Get<JwtOptions>();
+        builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
+        
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidAudience = jwtOptions.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
+                };
+            });
+    }
+    
+    public static void AddSwaggerGen(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference= new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id=JwtBearerDefaults.AuthenticationScheme
+                        }
+                    }, new string[]{}
+                }
+            });
+        });
+    }
+}
--- ChatManagement.API/ChatMessaging/Program.cs
+++ ChatManagement.API/ChatMessaging/Program.cs
@@ -1,10 +1,4 @@
-using System.Text;
-using ChatMessaging.Contracts;
-using ChatMessaging.DataAccess;
-using ChatMessaging.Implementations;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using ChatMessaging.Extensions;
 
 namespace ChatMessaging;
 
@@ -16,57 +10,12 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddAuthorization();
-        builder.Services.AddSingleton<ChatDbContext>();
-        builder.Services.AddScoped<IMessageRepository, MongoMessageRepository>();
-        builder.Services.AddSignalR();
+        builder.AddChatServices();
         
-        var jwtOptions = builder.Configuration.GetSection("ApiSettings:JwtOptions").Get<JwtOptions>();
-        builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
-        
-        builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtOptions.Issuer,
-                    ValidAudience = jwtOptions.Audience,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
-                };
-            });
+        builder.AddJwtAuth();
 
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(option =>
-        {
-            option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
-            {
-                Name = "Authorization",
-                Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
-                In = ParameterLocation.Header,
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer"
-            });
-            option.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference= new OpenApiReference
-                        {
-                            Type=ReferenceType.SecurityScheme,
-                            Id=JwtBearerDefaults.AuthenticationScheme
-                        }
-                    }, new string[]{}
-                }
-            });
-        });
+        builder.AddSwaggerGen();
         
         var app = builder.Build();
 
