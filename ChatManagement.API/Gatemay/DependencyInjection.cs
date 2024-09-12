1e0228b4de3b20caa18b2aa348e326662a89f153
TarasKvyk <tasik981@gmail.com>
2024-09-12 13:28:55 +0300

    Added Gateway

---
+++ ChatManagement.API/Gatemay/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Gatemay;
+
+public static class DependencyInjection
+{
+    public static void AddCors(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", builder =>
+                builder.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .SetIsOriginAllowed((host) => true));
+        });
+
+    }
+}
---
+++ ChatManagement.API/Gatemay/Program.cs
@@ -0,0 +1,32 @@
+using Gatemay;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Configuration
+    .AddJsonFile("ocelot.chats.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("ocelot.chathub.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+app.UseWebSockets();
+app.UseHttpsRedirection();
+app.UseOcelot().GetAwaiter().GetResult();
+
+app.Run();
